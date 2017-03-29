7112c591ca9190bd9eca5a8fa061dd9f83018d9c
KidKen <M@ur!c35>
2017-03-29 18:52:57 -0400

    Added Comments

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,16 @@ public class Main {
 
         long startTime = System.currentTimeMillis();
 
-
+        //Creates a robot object
         Robot robot = new Robot();
-
+        //Takes in the amount of clicks the user would like to do
         Scanner input = new Scanner(System.in);
         System.out.print("How many times would you like to press the mouse? ");
         int clicks = input.nextInt();
 
 
         try {
+            //Gives the user a 2 second delay before starting clicks
             Thread.sleep(2000);
 
 
@@ -26,9 +27,11 @@ public class Main {
                 robot.mouseRelease(InputEvent.BUTTON1_MASK);
                 robot.delay(90);
                 if (i % 10 == 0) {
+                    //Tells the user how many clicks have gone by so far can be easily changed from 10 to
+                    //whatever number
                     System.out.println(i + " of clicks so far");
                 }
-
+                    //Checks to see if the user is okay with finishing the clicks they initially wanted.
                 if (i % 100 == 0) {
                     System.out.print("Would you like to continue? (1/0)");
                     Scanner input1 = new Scanner(System.in);
@@ -41,7 +44,7 @@ public class Main {
 
                 }
             }
-
+            //Tells the user the amount of time it took for the Clicker to finish all the clicks
             long endTime = System.currentTimeMillis();
             long TotalTime = endTime - startTime;
             System.out.println(clicks + " clicks completed!");
